f83f7a678d7162f1dda3e2e6adabb95c41647954
hyhdb <kkminwoo@gmail.com>
2026-03-18 19:43:50 +0900

    3-08

--- src/main/java/com/mysite/sbb/answer/Answer.java
+++ src/main/java/com/mysite/sbb/answer/Answer.java
@@ -3,7 +3,14 @@ package com.mysite.sbb.answer;
 import java.time.LocalDateTime;
 
 import com.mysite.sbb.question.Question;
-import jakarta.persistence.*;
+import com.mysite.sbb.user.SiteUser;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -22,4 +29,7 @@ public class Answer {
 
     @ManyToOne
     private Question question;
+
+    @ManyToOne
+    private SiteUser author;
 }
